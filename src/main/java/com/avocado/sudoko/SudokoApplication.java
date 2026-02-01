7782f03b34d67e70b1603873b2e9014087e33690
wesamAlsaleh <wesammuneer7@gmail.com>
2026-02-01 19:48:05 +0300

    Init

---
+++ src/main/java/com/avocado/sudoko/SudokoApplication.java
@@ -0,0 +1,13 @@
+package com.avocado.sudoko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudokoApplication.class, args);
+    }
+
+}
