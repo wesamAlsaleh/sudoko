fad4c38448289c04ff9a1fff64bebdeb9124b170
wesamAlsaleh <wesammuneer7@gmail.com>
2026-02-03 01:32:52 +0300

    feat: add board printing functionality

--- src/main/java/com/avocado/sudoko/sudoku/Sudoku.java
+++ src/main/java/com/avocado/sudoko/sudoku/Sudoku.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 //@Entity
@@ -25,4 +26,58 @@ public class Sudoku {
     private String puzzleSolution;
 
     // todo: function to print the board
+    private void printSudoku(String puzzle) {
+        // create 2d 9x9 array
+        var board = new int[9][9];
+
+        // index flag
+        var stringIndex = 0;
+
+        // convert the string to 2D array
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                // get the value
+                board[i][j] = Character.getNumericValue(puzzle.charAt(stringIndex));
+
+                // update the pointer
+                stringIndex++;
+            }
+        }
+
+        // print in a nice way
+        for (int row = 0; row < 9; row++) {
+            // print divider after each 3 rows
+            if (row % 3 == 0) {
+                System.out.println("+-------+-------+-------+");
+            }
+            for (int col = 0; col < 9; col++) {
+                // print divider after each 3 columns
+                if (col % 3 == 0) {
+                    System.out.print("| ");
+                }
+                System.out.print(board[row][col] + " ");
+
+                // print the divider on the end of the column
+                if (col == 8) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println(); // go to next row after finishing from the column elements
+
+            // print the divider on the end of the row
+            if (row == 8) {
+                System.out.println("+-------+-------+-------+");
+            }
+        }
+    }
+
+    // function to print the sudoku solution
+    public void printSudokuSolution() {
+        printSudoku(this.puzzleSolution);
+    }
+
+    // function to print the sudoku board
+    public void printSudoku() {
+        printSudoku(this.puzzle);
+    }
 }
--- src/main/java/com/avocado/sudoko/sudoku/SudokuService.java
+++ src/main/java/com/avocado/sudoko/sudoku/SudokuService.java
@@ -16,6 +16,10 @@ public class SudokuService {
         // generate new sudoku game
         var sudoku = generator.generateSudokuGame(SudokuDifficulty.MEDIUM);
 
+        sudoku.printSudoku();
+        System.out.println("Sudoku solution: " + sudoku.getPuzzleSolution());
+        sudoku.printSudokuSolution();
+
         // return the sudoko as dto
         return sudokuMapper.toDto(sudoku);
     }
