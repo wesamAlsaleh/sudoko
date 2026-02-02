3933385df874fe86093a99c2252f2b4d4e0f5cfd
wesamAlsaleh <wesammuneer7@gmail.com>
2026-02-02 20:46:18 +0300

    feat: add validation logic for a Sudoku solver using Backtracking

--- src/main/java/com/avocado/sudoko/engine/Solver.java
+++ src/main/java/com/avocado/sudoko/engine/Solver.java
@@ -1,4 +1,73 @@
 package com.avocado.sudoko.engine;
 
+/**
+ * Implements the Backtracking algorithm logic for solving a Sudoku puzzle.
+ * Provides validation methods to check rows, columns, and 3x3 sub-grids
+ * to ensure no duplicate numbers exist.
+ */
 public class Solver {
+    // declare the grid size 9 x 9
+    private static final int GRID_SIZE = 9;
+
+    // this function is to check if the row contain repetition numbers
+    private boolean isNumberInRow(int[][] board, int row, int number) {
+        // iterate over the row elements (0 -> 8)
+        for (int i = 0; i < GRID_SIZE; i++) {
+            // if the number available return true
+            if (board[row][i] == number) {
+                return true;
+            }
+        }
+
+        // default
+        return false;
+    }
+
+    // this function is to check if the column contain repetition numbers
+    private boolean isNumberInColumn(int[][] board, int col, int number) {
+        // iterate over the row elements (0 -> 8)
+        for (int i = 0; i < GRID_SIZE; i++) {
+            // if the number available return true
+            if (board[i][col] == number) {
+                return true;
+            }
+        }
+
+        // default
+        return false;
+    }
+
+    // this function is to check if the 3 x 3 grid (box) contain repetition numbers
+    private boolean isNumberInGrid(int[][] board, int row, int col, int number) {
+        // get the grid start index (top-left of the box)
+        var localRowIndex = row - row % 3;
+        var localColIndex = col - col % 3;
+
+        // declare the box end boundary
+        var rowEnd = localRowIndex + 3;
+        var colEnd = localColIndex + 3;
+
+        // iterate over the grid
+        for (int i = localRowIndex; i < rowEnd; i++) {
+            for (int j = localColIndex; j < colEnd; j++) {
+                // if the number exist return true
+                if (board[i][j] == number) {
+                    return true;
+                }
+            }
+        }
+
+        // default
+        return false;
+    }
+
+    // function to check if the number is a valid number
+    private boolean isValidPlacement(int[][] board, int row, int col, int number) {
+        // return true if the number is not in the row, column, grid
+        return !isNumberInRow(board, row, number) &&
+                !isNumberInColumn(board, col, number) &&
+                !isNumberInGrid(board, row, col, number);
+    }
+
+    // function to
 }
--- src/main/java/com/avocado/sudoko/sudoku/Sudoku.java
+++ src/main/java/com/avocado/sudoko/sudoku/Sudoku.java
@@ -1,4 +1,18 @@
 package com.avocado.sudoko.sudoku;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
 public class Sudoku {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID uuid;
+
+//    private
+//    private String puzzle;
 }
